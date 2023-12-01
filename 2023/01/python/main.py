1bb7e5454074ece516e1f71a24fbd011bb06e93c
Hubert Lefevre <hubelefevre@gmail.com>
2023-12-01 23:45:04 +0100

    Part two: Python

--- 2023/01/python/main.py
+++ 2023/01/python/main.py
@@ -4,33 +4,39 @@
 
 from argparse import ArgumentParser, FileType
 
-DIGITS = [chr(x) for x in range(ord('0'), ord('9') + 1)]
+DIGITS_STR = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight',
+              'nine']
 
 
-def get_first_digit(line):
-    """Return the first digit we encounter in a given line."""
-    for x in line:
-        if x in DIGITS:
-            return x
+def get_digits(line, part_two):
+    """Return the digits encountered in a given line."""
+    ret = []
+    for i, x in enumerate(line):
+        if '0' <= x <= '9':
+            ret.append(ord(x) - ord('0'))
+        elif part_two:
+            for j, y in enumerate(DIGITS_STR):
+                if line[i:].startswith(y):
+                    ret.append(j + 1)
 
-    raise RuntimeError('No digit in line')
+    return ret
 
 
-def compute_line(line):
+def compute_line(line, part_two):
     """Process a line extracting first and last digit."""
-    first_digit = get_first_digit(line)
-    last_digit = get_first_digit(reversed(line))
-    return int(first_digit + last_digit, base=10)
+    digits = get_digits(line, part_two)
+    return digits[0] * 10 + digits[-1]
 
 
 def __main__():
     parser = ArgumentParser(description=__doc__)
 
     parser.add_argument(dest='input', type=FileType(encoding='utf-8'))
+    parser.add_argument('--part-two', action='store_true')
 
     args = parser.parse_args()
 
-    print(sum(compute_line(line) for line in args.input))
+    print(sum(compute_line(line, args.part_two) for line in args.input))
 
 
 if __name__ == '__main__':
