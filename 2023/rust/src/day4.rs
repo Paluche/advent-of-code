d45793f7024a9ecd634bf65f1df46c163aa5e43a
Hubert Lefevre <hubelefevre@gmail.com>
2023-12-08 21:35:53 +0100

    Day 4 part 1

---
+++ 2023/rust/src/day4.rs
@@ -0,0 +1,40 @@
+// Card   <id>: 10 numbers, each 2 digits | x (25) numbers 2 digits each.
+// Number could start with a space as first digit meaning 0.
+
+fn parse_numbers(s: &str) -> Vec<u8> {
+    let mut ret: Vec<u8> = Vec::new();
+
+    for number in s.split(' ') {
+        if let Ok(number) = number.parse::<u8>() {
+            ret.push(number)
+        }
+    }
+
+    ret
+}
+
+// Returns the number of winning numbers.
+fn parse_card(line: &str) -> usize {
+    let (_card_id, results) = line.split_once(':').unwrap();
+    let (winning, selected) = results.split_once('|').unwrap();
+
+    let winning = parse_numbers(winning);
+    parse_numbers(selected)
+        .iter()
+        .filter(|n| winning.contains(n))
+        .count()
+}
+
+fn part1_res(line: &str) -> usize {
+    let n = parse_card(line);
+    if n != 0 {
+        return 1 << (n - 1);
+    }
+
+    0
+}
+
+#[aoc(day4, part1)]
+fn part1(input: &str) -> usize {
+    input.lines().map(part1_res).sum()
+}
