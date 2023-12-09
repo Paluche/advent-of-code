6709ca97609461d9ce46b232ff652640493dc38a
Hubert Lefevre <hubelefevre@gmail.com>
2023-12-09 21:03:42 +0100

    Prefer split_whitespace() over split(' ')

--- 2023/rust/src/utils.rs
+++ 2023/rust/src/utils.rs
@@ -1,7 +1,7 @@
 pub fn parse_numbers<T: std::str::FromStr>(s: &str) -> Vec<T> {
     let mut ret: Vec<T> = Vec::new();
 
-    for number in s.split(' ') {
+    for number in s.split_whitespace() {
         if let Ok(number) = number.parse::<T>() {
             ret.push(number)
         }
