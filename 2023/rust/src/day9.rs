f2a61ce5554d51a96972a45431b47576c3839f79
Hubert Lefevre <hubelefevre@gmail.com>
2023-12-09 14:53:27 +0100

    day 9 part 2

--- 2023/rust/src/day9.rs
+++ 2023/rust/src/day9.rs
@@ -1,6 +1,6 @@
 use crate::utils;
 
-fn process_line(line: &str) -> isize {
+fn process_line_1(line: &str) -> isize {
     let mut numbers: Vec<isize> = utils::parse_numbers::<isize>(line);
     let mut ret: isize = 0;
 
@@ -12,7 +12,29 @@ fn process_line(line: &str) -> isize {
     ret
 }
 
+fn process_line_2(line: &str) -> isize {
+    let mut numbers: Vec<isize> = utils::parse_numbers::<isize>(line);
+    let mut data: Vec<isize> = Vec::new();
+
+    while numbers.iter().any(|x| *x != 0) {
+        data.push(numbers[0]);
+        numbers = numbers.windows(2).map(|t| t[1] - t[0]).collect()
+    }
+
+    let mut ret: isize = 0;
+    for x in data.iter().rev() {
+        ret = x - ret;
+    }
+
+    ret
+}
+
 #[aoc(day9, part1)]
 fn part1(input: &str) -> isize {
-    input.lines().map(process_line).sum()
+    input.lines().map(process_line_1).sum()
+}
+
+#[aoc(day9, part2)]
+fn part2(input: &str) -> isize {
+    input.lines().map(process_line_2).sum()
 }
