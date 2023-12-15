4c6f86d1682fcca60f459ebdf4b4a5c79d5c4542
Hubert Lefevre <hubelefevre@gmail.com>
2023-12-15 12:07:30 +0100

    day 15 part 2

--- 2023/rust/src/day15.rs
+++ 2023/rust/src/day15.rs
@@ -1,22 +1,68 @@
-fn hash_algorithm(c:char, current_value: &mut usize) {
-    let mut ret:usize = *current_value as usize;
+fn hash_algorithm(s: &str) -> usize {
+    let mut ret: usize = 0;
 
-    if c == '\n' {
-        return;
+    for c in s.chars() {
+        if c == '\n' {
+            continue;
+        }
+
+        ret += c as usize;
+        ret *= 17;
+        ret %= 256;
     }
-
-    ret += c as usize;
-    ret *= 17;
-    *current_value = ret % 256;
+    ret
 }
 
 #[aoc(day15, part1)]
 fn part1(input: &str) -> usize {
-    input.split(',').map(|s| {
-        let mut ret: usize = 0;
-        for c in s.chars() {
-            hash_algorithm(c, &mut ret);
+    input.split(',').map(hash_algorithm).sum()
+}
+
+fn parse_entry<'a>(s: &'a str, boxes: &mut Vec<Vec<(&'a str, usize)>>) {
+    let s = s.trim_end();
+
+    if s == "" {
+        return;
+    }
+
+    if s.chars().last().unwrap() == '-' {
+        let label = &s[..s.len() - 1];
+        let id = hash_algorithm(label);
+
+        if let Some(position) = boxes[id].iter().position(|(l, _)| *l == label)
+        {
+            boxes[id].remove(position);
         }
-        ret
-    }).sum::<usize>()
+    } else {
+        let (label, focal) = s.split_once('=').unwrap();
+        let id = hash_algorithm(label);
+        let focal = focal.parse::<usize>().unwrap();
+
+        if let Some(position) = boxes[id].iter().position(|(l, _)| *l == label)
+        {
+            boxes[id].remove(position);
+            boxes[id].insert(position, (label, focal));
+        } else {
+            boxes[id].push((label, focal));
+        }
+    }
+}
+
+#[aoc(day15, part2)]
+fn part2(input: &str) -> usize {
+    let mut boxes: Vec<Vec<(&str, usize)>> = vec![Vec::new(); 256];
+
+    input.split(',').for_each(|s| parse_entry(s, &mut boxes));
+
+    boxes
+        .iter()
+        .enumerate()
+        .map(|(n, lenses)| {
+            lenses
+                .iter()
+                .enumerate()
+                .map(|(i, (_, f))| (n + 1) * (i + 1) * f)
+                .sum::<usize>()
+        })
+        .sum()
 }
