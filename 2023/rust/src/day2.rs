6bbeecf2527ef901da24db1a0eba21c0682f264a
Hubert Lefevre <hubelefevre@gmail.com>
2023-12-15 13:22:31 +0100

    day 2 factoring

--- 2023/rust/src/day2.rs
+++ 2023/rust/src/day2.rs
@@ -1,114 +1,64 @@
 // In the bag only 12 red cubes, 13 green cubes, and 14 blue cubes.
-static RED_CUBES_MAX: usize = 12;
-static GREEN_CUBES_MAX: usize = 13;
-static BLUE_CUBES_MAX: usize = 14;
+static MAX: [(&str, usize); 3] = [("red", 12), ("green", 13), ("blue", 14)];
 
-fn str_to_usize(s: &str) -> Option<usize> {
-    match s.parse::<usize>() {
-        Ok(n) => Some(n),
-        Err(_) => None,
+fn str_to_usize(s: &str) -> usize {
+    s.parse::<usize>().unwrap()
+}
+
+fn is_doable(line: &str) -> usize {
+    let (key, value) = line.split_once(':').unwrap();
+
+    if value.split(';').any(set_fails) {
+        0
+    } else {
+        str_to_usize(&key["Game ".len()..])
     }
 }
 
-fn is_doable(line: &str) -> Option<usize> {
-    // Split game title, from content.
-    let (key, value) = line.split_once(':')?;
-    // Skip string "Game "
-    let i = "Game ".len();
-    let game_number = str_to_usize(&key[i..]);
-
-    // Split the content in sets.
-    for set in value.split(';') {
-        if !check_set(set)? {
-            return Some(0);
-        }
-    }
-
-    game_number
+fn set_fails(line: &str) -> bool {
+    line.split(',').any(|s| {
+        let (count, id) = s[1..].split_once(' ').unwrap();
+        let count = str_to_usize(count);
+        MAX.iter().any(|(c, m)| id == *c && count > *m)
+    })
 }
 
-fn check_set(line: &str) -> Option<bool> {
-    for s in line.split(',') {
-        let (count, id) = s[1..].split_once(' ')?;
-        let count = str_to_usize(count)?;
-
-        if id == "red" && count > RED_CUBES_MAX {
-            return Some(false);
-        }
-
-        if id == "green" && count > GREEN_CUBES_MAX {
-            return Some(false);
-        }
-
-        if id == "blue" && count > BLUE_CUBES_MAX {
-            return Some(false);
-        }
-    }
-
-    Some(true)
+fn minimal_cubes(line: &str) -> usize {
+    line.split_once(':')
+        .unwrap()
+        .1
+        .split(';')
+        .fold([0; 3], |mut x, set| {
+            for (i, color) in used_balls(set).iter().enumerate() {
+                if x[i] < *color {
+                    x[i] = *color;
+                }
+            }
+            x
+        })
+        .iter()
+        .product()
 }
 
-fn minimal_cubes(line: &str) -> Option<usize> {
-    // Remove game title, from content.
-    let start = line.find(':')? + 1;
-    let value = &line[start..];
-
-    // Split the content in sets.
-    let mut res: (usize, usize, usize) = (0, 0, 0);
-    for set in value.split(';') {
-        let (red, green, blue) = used_balls(set)?;
-
-        if res.0 < red {
-            res.0 = red;
-        }
-
-        if res.1 < green {
-            res.1 = green;
-        }
-
-        if res.2 < blue {
-            res.2 = blue;
-        }
-    }
-
-    Some(res.0 * res.1 * res.2)
-}
-
-fn used_balls(line: &str) -> Option<(usize, usize, usize)> {
-    let mut ret: (usize, usize, usize) = (0, 0, 0);
-
-    for s in line.split(',') {
-        let (count, id) = s[1..].split_once(' ')?;
-        let count = str_to_usize(count)?;
-
-        if id == "red" {
-            ret.0 = count
-        }
-
-        if id == "green" {
-            ret.1 = count
-        }
-
-        if id == "blue" {
-            ret.2 = count
-        }
-    }
-
-    Some(ret)
+fn used_balls(line: &str) -> [usize; 3] {
+    line.split(',').fold([0; 3], |mut x, s| {
+        let (count, id) = s[1..].split_once(' ').unwrap();
+        x[match id {
+            "red" => 0,
+            "green" => 1,
+            "blue" => 2,
+            _ => panic!(),
+        }] = str_to_usize(count);
+        x
+    })
 }
 
 #[aoc(day2, part1)]
 fn part1(input: &str) -> usize {
-    input
-        .lines()
-        .map(|l| -> usize { is_doable(l).unwrap() })
-        .sum()
+    input.lines().map(is_doable).sum()
 }
 
 #[aoc(day2, part2)]
 fn part2(input: &str) -> usize {
-    input
-        .lines()
-        .map(|l| -> usize { minimal_cubes(l).unwrap() })
-        .sum()
+    input.lines().map(minimal_cubes).sum()
 }
