20030a23daef87e354240fbaad934e031f23cefe
Hubert Lefevre <hubelefevre@gmail.com>
2023-12-15 13:36:39 +0100

    day4 clean

--- 2023/rust/src/day4.rs
+++ 2023/rust/src/day4.rs
@@ -1,39 +1,40 @@
-use crate::utils;
+use crate::utils::parse_numbers;
 // Card   <id>: 10 numbers, each 2 digits | x (25) numbers 2 digits each.
 // Number could start with a space as first digit meaning 0.
 
 // Returns the number of winning numbers.
 fn parse_card(line: &str) -> usize {
-    let (_card_id, results) = line.split_once(':').unwrap();
-    let (winning, selected) = results.split_once('|').unwrap();
+    let (winning, selected) =
+        line.split_once(':').unwrap().1.split_once('|').unwrap();
 
-    let winning = utils::parse_numbers::<u8>(winning);
-    utils::parse_numbers(selected)
+    let winning = parse_numbers::<u8>(winning);
+
+    parse_numbers(selected)
         .iter()
         .filter(|n| winning.contains(n))
         .count()
 }
 
-fn part1_res(line: &str) -> usize {
-    let n = parse_card(line);
-    if n != 0 {
-        return 1 << (n - 1);
-    }
-
-    0
-}
-
 #[aoc(day4, part1)]
 fn part1(input: &str) -> usize {
-    input.lines().map(part1_res).sum()
+    input
+        .lines()
+        .map(|l| {
+            let n = parse_card(l);
+            if n != 0 {
+                1 << (n - 1)
+            } else {
+                0
+            }
+        })
+        .sum()
 }
 
 #[aoc(day4, part2)]
 fn part2(input: &str) -> usize {
-    let cards_results = input.lines().map(parse_card);
     let mut cards_copies = vec![1; input.lines().count()];
 
-    for (i, result) in cards_results.enumerate() {
+    for (i, result) in input.lines().map(parse_card).enumerate() {
         let cur_copies = cards_copies[i];
 
         for j in i + 1..i + 1 + result {
