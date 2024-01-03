0d1b0f1352b89f680504637c383505bdc319f851
Hubert Lefevre <hubelefevre@gmail.com>
2024-01-03 21:46:18 +0100

    2016: day 5

---
+++ 2016/rust/src/day5.rs
@@ -0,0 +1,51 @@
+fn run<F>(input: &str, mut f: F) -> String
+where
+    F: FnMut(u8, u8, usize, &mut [u8; 8]) -> usize,
+{
+    let input = input.lines().next().unwrap();
+    let mut ret = [0xff_u8; 8];
+    let mut size = 0;
+
+    for index in 0.. {
+        let mut input = String::from(input);
+        input.push_str(index.to_string().as_str());
+
+        let digest = md5::compute(input);
+
+        if digest[0] == 0x00 && digest[1] == 0x00 && (digest[2] >> 4) == 0x0 {
+            size = f(digest[2] & 0xf, digest[3] >> 4, size, &mut ret);
+
+            if size == 8 {
+                break;
+            }
+        }
+    }
+
+    let mut s = String::with_capacity(8);
+
+    for x in ret {
+        s.push(format!("{x:x}").chars().next().unwrap());
+    }
+
+    s
+}
+
+#[aoc(day5, part1)]
+fn part1(input: &str) -> String {
+    run(input, |a, _, size, ret| {
+        ret[size] = a;
+        size + 1
+    })
+}
+
+#[aoc(day5, part2)]
+fn part2(input: &str) -> String {
+    run(input, |a, b, size, ret| {
+        if (0..=7).contains(&a) && ret[a as usize] == 0xff {
+            ret[a as usize] = b;
+            size + 1
+        } else {
+            size
+        }
+    })
+}
